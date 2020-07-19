bbecd059df9ce64a8ab48e89053b9d30da8750fe
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2020-07-19 08:49:42 -0400

    changed port to 8080

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -57,7 +57,7 @@ app.use(function(err, req, res, next) {
     }
 });
 
-const port = process.env.PORT || 8085;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`A Node Js API is listening on port: ${port}`);
 });
